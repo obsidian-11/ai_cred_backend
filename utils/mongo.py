5f77b2f2f27bb5b871f542096a106633782739cd
Syed <syedaneesibrahom@gmail.com>
2026-02-21 09:49:08 +0000

    mongo connection changes

--- utils/mongo.py
+++ utils/mongo.py
@@ -1,11 +1,12 @@
-from pymongo import MongoClient
+# utils/mongo.py
 import os
+from pymongo import MongoClient
 from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv(os.path.join(os.path.dirname(__file__), "../.env"))
 
 client = MongoClient(os.getenv("MONGO_URI"))
-db = client["ai_cred_db"]
+db = client[os.getenv("DB_NAME", "ai_cred_db")]
 
 def get_collection(name="sources"):
     return db[name]
